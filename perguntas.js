a8494ddf18c6be1b59fd7edb093838ff1734e4a9
Menezes <145766384+yluizx@users.noreply.github.com>
2024-09-30 12:13:54 -0300

    luiz

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'conjunto das regras e procedimentos lógicos perfeitamente definidos que levam à solução de um problema em um número finito de etapas.'
+    )
+    criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+    )
+    criaCartao(
+    'Programação',
+    'O que é lógica de programação?',
+    'Conjunto de regras e técnicas que os programadores utilizam para projetar e desenvolver programas de computador.'
+    )
+    criaCartao(
+    'Lingua inglesa',
+    'Como se diz cabelo em Inglês?',
+    'Oi em ingles é HAIR (HAIR)'
+    )
